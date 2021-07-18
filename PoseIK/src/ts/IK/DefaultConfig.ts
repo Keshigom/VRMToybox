b2877844da09a6a98aafc7fbad0dd64e2ec81389
Keshigom <16793195+Keshigom@users.noreply.github.com>
2021-07-19 02:17:26 +0900

    関節角度制限

--- PoseIK/src/ts/IK/DefaultConfig.ts
+++ PoseIK/src/ts/IK/DefaultConfig.ts
@@ -27,28 +27,109 @@ example
 
 // VRM0.xの必須ボーンのみで構成
 export const defaultIKConfig: IKConfig = {
-    iteration: 128,
+    iteration: 8,
     chainConfigs: [
-        // Left Hand
+        // Hip -> Head
+        {
+            jointConfigs: [
+
+                {
+                    boneName: VRMSchema.HumanoidBoneName.Neck,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.Chest,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.Spine,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.Hips,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+            ],
+            effectorBoneName: VRMSchema.HumanoidBoneName.Head
+        },
+        // Left Shoulder -> Hand
         {
             jointConfigs: [
                 {
                     boneName: VRMSchema.HumanoidBoneName.LeftLowerArm,
-                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
-                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                    rotationMin: new Vector3(0, -Math.PI, 0),
+                    rotationMax: new Vector3(Math.PI, 0, Math.PI),
                 },
                 {
                     boneName: VRMSchema.HumanoidBoneName.LeftUpperArm,
-                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
-                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                    rotationMin: new Vector3(-(120 / 180) * Math.PI, 0, 0),
+                    rotationMax: new Vector3((120 / 180) * Math.PI, 0, (45 / 180) * Math.PI),
                 },
                 {
                     boneName: VRMSchema.HumanoidBoneName.LeftShoulder,
-                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
-                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                    rotationMin: new Vector3(0, 0, -(45 / 180) * Math.PI),
+                    rotationMax: new Vector3(0, 0, 0),
                 }
             ],
             effectorBoneName: VRMSchema.HumanoidBoneName.LeftHand
-        }
+        },
+        // Right Shoulder -> Hand
+        {
+            jointConfigs: [
+                {
+                    boneName: VRMSchema.HumanoidBoneName.RightLowerArm,
+                    rotationMin: new Vector3(0, 0, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, 0),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.RightUpperArm,
+                    rotationMin: new Vector3(-(120 / 180) * Math.PI, 0, 0),
+                    rotationMax: new Vector3((120 / 180) * Math.PI, 0, (45 / 180) * Math.PI),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.RightShoulder,
+                    rotationMin: new Vector3(0, 0, 0),
+                    rotationMax: new Vector3(0, 0, (45 / 180) * Math.PI),
+                },
+            ],
+            effectorBoneName: VRMSchema.HumanoidBoneName.RightHand
+        },
+        // Left Leg
+        {
+            jointConfigs: [
+                {
+                    boneName: VRMSchema.HumanoidBoneName.LeftLowerLeg,
+                    rotationMin: new Vector3(-Math.PI, 0, 0),
+                    rotationMax: new Vector3(0, 0, 0),
+
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.LeftUpperLeg,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+            ],
+            effectorBoneName: VRMSchema.HumanoidBoneName.LeftFoot
+        },
+        // Right Leg
+        {
+            jointConfigs: [
+                {
+                    boneName: VRMSchema.HumanoidBoneName.RightLowerLeg,
+                    rotationMin: new Vector3(-Math.PI, 0, 0),
+                    rotationMax: new Vector3(0, 0, 0),
+                },
+                {
+                    boneName: VRMSchema.HumanoidBoneName.RightUpperLeg,
+                    rotationMin: new Vector3(-Math.PI, -Math.PI, -Math.PI),
+                    rotationMax: new Vector3(Math.PI, Math.PI, Math.PI),
+                },
+            ],
+            effectorBoneName: VRMSchema.HumanoidBoneName.RightFoot
+        },
     ]
 };
--- PoseIK/src/ts/UI.ts
+++ PoseIK/src/ts/UI.ts
@@ -8,9 +8,6 @@ export const setupIKController = (viewer: Viewer, avatar: Avatar) => {
 
     avatar.vrmIK.ikChains.forEach(chain => {
         // test Obj
-        const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
         const transCtrl = new TransformControls(viewer.camera, viewer.canvas);
 
         transCtrl.attach(chain.goal);
@@ -18,7 +15,6 @@ export const setupIKController = (viewer: Viewer, avatar: Avatar) => {
             viewer.orbitControl.enabled = !event.value;
         });
         viewer.scene.add(transCtrl);
-        chain.goal.add(cube);
         // 
     });
 }
