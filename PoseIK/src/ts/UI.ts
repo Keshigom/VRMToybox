b26609173f3b5798e306ffef838dce7e3a5b72d0
Keshigom <16793195+Keshigom@users.noreply.github.com>
2021-07-23 18:36:58 +0900

    fix:コントローラがアバター変更時に破棄されるように修正

--- PoseIK/src/ts/UI.ts
+++ PoseIK/src/ts/UI.ts
@@ -7,14 +7,12 @@ import { Viewer } from './Viewer';
 export const setupIKController = (viewer: Viewer, avatar: Avatar) => {
 
     avatar.vrmIK.ikChains.forEach(chain => {
-        // test Obj
         const transCtrl = new TransformControls(viewer.camera, viewer.canvas);
 
         transCtrl.attach(chain.goal);
         transCtrl.addEventListener('dragging-changed', event => {
             viewer.orbitControl.enabled = !event.value;
         });
-        viewer.scene.add(transCtrl);
-        // 
+        avatar.vrm.scene.add(transCtrl);
     });
 }
